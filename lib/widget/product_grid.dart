21da3b2751af3435d20d933a813640335f6520be
muzz-code <uyihope360@gmail.com>
2023-01-24 12:35:28 +0100

    set up the providers

---
+++ lib/widget/product_grid.dart
@@ -0,0 +1,27 @@
+import 'package:example/providers/products.dart';
+import 'package:example/widget/product_item.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:provider/provider.dart';
+
+import '../models/product.dart';
+
+class ProductData extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    final productData = Provider.of<Products>(context);
+    final product = productData.item;
+    return GridView.builder(
+        padding: const EdgeInsets.all(10),
+        itemCount: product.length,
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisCount: 2,
+            childAspectRatio: 3 / 2,
+            crossAxisSpacing: 10,
+            mainAxisSpacing: 10),
+        itemBuilder: (ctx, i) => ProductItem(
+            imageUrl: product[i].imageUrl,
+            id: product[i].id,
+            title: product[i].title));
+  }
+}
